791797a27a6f77f18ce58d855adea63a2499086c
dzhengiz.turhan@outlook.com <dzhengiz.turhan@outlook.com>
2024-11-23 22:25:41 +0200

    Add project files.

---
+++ InventoryManagementSystem/Data/Item.cs
@@ -0,0 +1,46 @@
+﻿using InventoryManagementSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Data
+{
+    public class Item : IItem, ICategorizable
+    {
+        private string name;
+        private double price;
+        private int quantity;
+        private string category;
+        public Item(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public double CalculateValue()
+        {
+            return quantity * price;
+        }
+
+        public string GetItemDetails()
+        {
+            return $"Name: {name}, Price: {price}, Quantity: {quantity}";
+        }
+
+        public void ItemDescription()
+        {
+            Console.WriteLine($"Description: {name}, Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+        }
+        public string GetCategory()
+        {
+            return category;
+        }
+        public void SetCategory(string categoryName)
+        {
+            this.category = categoryName;
+        }
+    }
+}
---
+++ InventoryManagementSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagementSystem.Data;
+using InventoryManagementSystem.Interfaces;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IItem item = new Item("Lenovo",1000,2);
+        IItem item2 = new Item("Samsung 24 Ultra",2200,3);
+
+        Console.WriteLine(item.GetItemDetails());
+        item.ItemDescription();
+        Console.WriteLine(($"Total: {item.CalculateValue()}"));
+
+
+        Console.WriteLine(item2.GetItemDetails());
+        item2.ItemDescription();
+        Console.WriteLine($"Total: {item2.CalculateValue()}");
+    }
+}
